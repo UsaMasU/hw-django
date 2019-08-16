91e4721f494d03d4caa19581ec149a24a3241515
UsaMasU <v_v_semenov@mail.ru>
2019-08-16 17:55:50 +0300

    HW_2 ex.3

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,12 +1,16 @@
 from django import template
-
-
+import datetime
+import time
 register = template.Library()
+
 
 
 @register.filter
 def format_date(value):
     # Ваш код
+    #print(value)
+    dt = datetime.datetime.fromtimestamp(value).timetuple()
+    print('post time:', dt)
     return value
 
 
