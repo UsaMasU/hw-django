f83c3747317af9adda75d0046da545cd05ed6b4f
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-09-01 17:22:25 +0300

    HW #4 ex.3

--- databases_2/orm_migrations/school/admin.py
+++ databases_2/orm_migrations/school/admin.py
@@ -3,11 +3,30 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    pass
+class MembershipInline(admin.TabularInline):
+    model = Student.teachers.through
+    extra = 1
 
 
-@admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    inlines = [MembershipInline, ]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline, ]
+    exclude = ('teachers',)
+
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Student, StudentAdmin)
+
+
+
+# @admin.register(Student)
+# class StudentAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Teacher)
+# class TeacherAdmin(admin.ModelAdmin):
+#     pass
--- databases_2/orm_migrations/school/urls.py
+++ databases_2/orm_migrations/school/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
+from django.conf import settings
+from django.urls import include
+
 from school.views import students_list
+
 
 urlpatterns = [
     path('', students_list, name='students'),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
