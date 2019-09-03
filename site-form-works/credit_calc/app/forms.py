0d07e3cbc41c1f8cf49284602e3f68060c4096e5
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-09-03 20:11:00 +0300

    HW #5 ex.1

--- site-form-works/credit_calc/app/forms.py
+++ site-form-works/credit_calc/app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class CalcForm(forms.Form):
     initial_fee = forms.IntegerField(label="Стоимость товара")
-    rate = forms.CharField(label="Процентная ставка")
+    rate = forms.IntegerField(label="Процентная ставка")
     months_count = forms.IntegerField(label="Срок кредита в месяцах")
 
     def clean_initial_fee(self):
@@ -13,6 +13,18 @@ class CalcForm(forms.Form):
             raise forms.ValidationError("Стоимость товара не может быть отрицательной")
         return initial_fee
 
+    def clean_rate(self):
+        rate = self.cleaned_data.get('rate')
+        if not rate  or rate  < 0:
+            raise forms.ValidationError("Процентная ставка не может быть отрицательной")
+        return rate
+
+
+    def clean_months_count(self):
+        months_count = self.cleaned_data.get('months_count')
+        if not months_count or months_count  < 0:
+            raise forms.ValidationError("Необходимо указать количество месяцев")
+        return months_count
+
     def clean(self):
-        # общая функция валидации
         return self.cleaned_data
--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
-
 from .forms import CalcForm
-
 
 def calc_view(request):
     template = "app/calc.html"
 
-    form = CalcForm
-    context = {
-        'form': form
-    }
+    get_cost = request.GET
 
-    return render(request, template, context)
+    if len(get_cost) > 0:
+       common_result = (float(get_cost['initial_fee']) + float(get_cost['initial_fee']) * float(get_cost['rate']) / float(get_cost['months_count']))
+       result = common_result  / 12
+       result = '{:.3f}'.format(result)
+       common_result = '{:.2f}'.format(common_result)
+
+       form = CalcForm(request.GET)
+       if form.is_valid():
+           context = {
+               'form': form,
+               'result': result,
+               'common_result': common_result
+           }
+       else:
+           context = {
+               'form': form,
+               'result': None,
+               'common_result': None
+           }
+       return render(request, template, context)
+    else:
+        form = CalcForm
+        context = {
+            'form': form
+        }
+        return render(request, template, context)
