dfb5f9f167b4f0ab506d44b0ed0c7d6a72dcab81
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-09-04 07:27:49 +0300

    HW #5 ex.2

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -8,6 +8,7 @@ from .forms import ReviewForm
 def product_list_view(request):
     template = 'app/product_list.html'
     products = Product.objects.all()
+    print('products view')
 
     context = {
         'product_list': products,
@@ -17,17 +18,30 @@ def product_list_view(request):
 
 
 def product_view(request, pk):
+    print('product')
     template = 'app/product_detail.html'
-    product = get_object_or_404(Product, id=pk)
 
-    form = ReviewForm
     if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+        print('post')
+        product = get_object_or_404(Product, id=pk)
 
-    context = {
-        'form': form,
-        'product': product
-    }
+        form = ReviewForm
+        if request.method == 'POST':
+            # логика для добавления отзыва
+            pass
 
+        context = {
+            'form': form,
+            'product': product
+        }
+        return render(request, template, context)
+    else:
+        form = ReviewForm
+        product = {}
+        context = {
+            'form': form,
+            'product': product
+        }
     return render(request, template, context)
+
+
