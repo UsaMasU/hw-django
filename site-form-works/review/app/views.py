f309105b9c767e1d80d57dd4d2751f65294dc03a
UsaMasU <v_v_semenov@mail.ru>
2019-09-04 17:25:21 +0300

    HW_5 ex.2 v.2

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -8,40 +8,46 @@ from .forms import ReviewForm
 def product_list_view(request):
     template = 'app/product_list.html'
     products = Product.objects.all()
-    print('products view')
+
+    if not('reviewed_products' in request.session):
+        request.session['reviewed_products'] = []
+    #del request.session['reviewed_products']
 
     context = {
-        'product_list': products,
+        'product_list': products
     }
 
     return render(request, template, context)
 
 
 def product_view(request, pk):
-    print('product')
     template = 'app/product_detail.html'
+    product = get_object_or_404(Product, id=pk)
 
     if request.method == 'POST':
-        print('post')
-        product = get_object_or_404(Product, id=pk)
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = Review(text=form.cleaned_data['text'], product=product)
+            review.save()
 
-        form = ReviewForm
-        if request.method == 'POST':
-            # логика для добавления отзыва
-            pass
+            session_store = request.session['reviewed_products']
+            session_store.append(product.name)
+            request.session['reviewed_products'] = session_store
 
-        context = {
-            'form': form,
-            'product': product
-        }
-        return render(request, template, context)
+            return redirect(reverse('product_detail', args=[product.pk]))
     else:
         form = ReviewForm
-        product = {}
+        reviews = Review.objects.all().filter(product=product)
+
+        is_review_exist = True if request.session['reviewed_products'].count(product.name) >= 1 else False
+
         context = {
             'form': form,
-            'product': product
+            'product': product,
+            'reviews': reviews,
+            'is_review_exist': is_review_exist
         }
-    return render(request, template, context)
+        return render(request, template, context)
+
 
 
