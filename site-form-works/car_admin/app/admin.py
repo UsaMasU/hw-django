ced0939a3e85356224d518a36a61b6571a38b8ec
UsaMasU <v_v_semenov@mail.ru>
2019-09-05 18:27:52 +0300

    HW_5 ex.2 v.4

--- site-form-works/car_admin/app/admin.py
+++ site-form-works/car_admin/app/admin.py
@@ -5,7 +5,15 @@ from .forms import ReviewAdminForm
 
 
 class CarAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ('model', 'brand', 'review_count')
+    list_filter = ('model', 'brand',)
+
+
+class CarInline(admin.StackedInline):
+    model = Car
+    extra = 0
+    ordering = ('-id',)
 
 
 class ReviewAdmin(admin.ModelAdmin):
