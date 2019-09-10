bca416761abc4d1ff5e28e1220da4dd96fdc8c8c
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-09-10 08:06:44 +0300

    HW #6 ex.2_1

--- site-personalization/sessions/game/models.py
+++ site-personalization/sessions/game/models.py
@@ -2,12 +2,26 @@ from django.db import models
 
 
 class Player(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name='Имя')
+
+    def __str__(self):
+        return self.name
 
 
 class Game(models.Model):
-    pass
+    session_name = models.CharField(max_length=256, verbose_name='ID сессии')
+    number = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, verbose_name='Статус игры')
+    players = models.ManyToManyField(Player, through='PlayerGameInfo', verbose_name='Игроки')
+
+    def __str__(self):
+        return self.session_name
 
 
 class PlayerGameInfo(models.Model):
-    pass
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, verbose_name='Игрок')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, verbose_name='Игра')
+    attempts = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{0}_{1}'.format(self.player, self.game)
--- site-personalization/sessions/game/views.py
+++ site-personalization/sessions/game/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+import random
 
 
 def show_home(request):
+
+    x_number = random.randint(0, 100)
+
+    my_car = request.session.get('x_number_game', 'start')
+
     return render(
         request,
         'home.html'
