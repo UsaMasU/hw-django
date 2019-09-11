3353ccecce2033fd5764a6fea8794dde80cd01ad
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-09-11 20:28:41 +0300

    HW #6 ex.2_3

--- site-personalization/sessions/game/views.py
+++ site-personalization/sessions/game/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 from .models import Player, Game, PlayerGameInfo
 
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
 import random
 
 
 def show_home(request):
     number = -1
-    #del  request.session['x_number_game']
-    #return False
-
     if request.method == 'POST':
         number = request.POST.get("enter_number")
         number = int(number)
-        print('entered number:', number)
 
     if request.session.has_key('x_number_game'):
         session_data = request.session.get('x_number_game')
-        print('active session:', session_data)
-        game_state = session_data['game_state']
-        player_id = session_data['player_id']
+        game = Game.objects.get(id=session_data['game_id'])
+        player_game_info = PlayerGameInfo.objects.get(game_id=session_data['game_id'])
+
+        if game.state == 'game over':
+            del request.session['x_number_game']
+            text = f'ваше число: {str(game.x_number)} угадали с {str(player_game_info.attempts)} попыток'
+            context = {
+                'text': text
+            }
+            return render(request, 'home.html', context)
         game_id = session_data['game_id']
         x_number = session_data['game_x_number']
         attempts = session_data['attempts']
@@ -33,30 +33,21 @@ def show_home(request):
             if number is not -1:
                 attempts += 1
                 if number > x_number:
-                    print('Больше')
                     text = 'Загаданное число Меньше. Угадайте число:'
                 elif number < x_number:
-                    print('Меньше')
                     text = 'Загаданное число Больше. Угадайте число:'
                 else:
-                    print('Рввно')
                     text = 'ДА!'
                     role = 'winner'
                     game = Game.objects.get(id=game_id)
-                    game.state = 'over'
+                    game.state = 'game over'
                     game.save()
-                    game_state = game.state
-                    request.session['x_number_game']['game_state'] = game.state
-
-            print('state', game_state)
-            if game_state == 'playing':
-                print('yes - playing')
-                request.session['x_number_game']['attempts'] = attempts
-                request.session.modified = True
-            else:
-                del request.session['x_number_game']
-                #return HttpResponseRedirect(reverse('home'))
-
+                    request.session['x_number_game']['game_state'] = 'game over'
+            request.session['x_number_game']['attempts'] = attempts
+            player_game_info = PlayerGameInfo.objects.get(game_id=session_data['game_id'])
+            player_game_info.attempts = attempts
+            player_game_info.save()
+            request.session.modified = True
             context = {
                 'role': role,
                 'text': text,
@@ -65,7 +56,7 @@ def show_home(request):
             }
             return render(request, 'home.html', context)
         else:
-            text = 'Ваше число разгадывают'
+            text = f'Число {str(game.x_number)} сейчас пытаются угадать'
             context = {
                 'role': role,
                 'text': text,
@@ -73,22 +64,13 @@ def show_home(request):
             }
             return render(request, 'home.html', context)
     else:
-        print('no session')
         games = Game.objects.all()
-        print(games)
-
         if len(games) > 0:
             for game in games:
-                print('game:', game.state)
                 if game.state == 'playing':
-                    print('active game:', game.id)
-                    print('x:', game.x_number)
-                    print('state:', game.state)
-
                     player = Player(name='Player_Finder')
                     player.save()
                     role = 'finder'
-
                     game_data = {
                         'player_id': player.id,
                         'role': role,
@@ -97,9 +79,7 @@ def show_home(request):
                         'game_x_number': game.x_number,
                         'attempts': 0
                     }
-                    print(game_data)
                     request.session['x_number_game'] = game_data
-
                     text = 'Угадайте число'
                     context = {
                         'role': role,
@@ -108,20 +88,13 @@ def show_home(request):
                     }
                     return render(request, 'home.html', context)
 
-        print('no active game')
         player = Player(name='Player_Creator')
         player.save()
         x_number = random.randint(0, 100)
         game = Game(x_number=x_number, state='playing')
         game.save()
         player_game_info = PlayerGameInfo.objects.create(player=player, game=game, attempts=0)
-        print(player_game_info)
-
-        print('player:', player.id)
-        print('game:', game.id)
-        print('player_game_info:', player_game_info.id)
         role = 'creator'
-
         game_data = {
             'player_id': player.id,
             'role' : role,
@@ -130,10 +103,9 @@ def show_home(request):
             'game_x_number': x_number,
             'attempts': 0
         }
-        print(game_data)
         request.session['x_number_game'] = game_data
 
-        text = 'Начинаем игру'
+        text = f'Начинаем игру. Загаданное число: {str(game.x_number)}'
         context = {
             'role': role,
             'text': text,
