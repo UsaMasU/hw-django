48511e564a43c491fdfc87add3fef98477ce19d2
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2019-09-11 22:27:12 +0300

    HW #6 ex.3

--- site-personalization/auth_and_reg_users/main/urls.py
+++ site-personalization/auth_and_reg_users/main/urls.py
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path
 
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
 import auth.views
 
 urlpatterns = [
-    path('', auth.views.home),
-    path('admin/', admin.site.urls),
-    path('signup/', auth.views.signup),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^signup/$', auth.views.RegisterFormView.as_view()),
+    url(r'^login/$', auth.views.LoginFormView.as_view()),
+    url(r'^logout/$', auth.views.LogoutView.as_view()),
 ]
